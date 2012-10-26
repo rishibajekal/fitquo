651b969cd340535386cfd36e00040a3c34b4dac9
Mohd Irtefa <irtefa1@illinois.edu>
2012-10-26 01:18:59 -0500

    fixed query_results.js

--- static/js/query_results.js
+++ static/js/query_results.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $.getJSON('/api/search', function(data) {
-    $.each(data, function(email, name) {
-      $('#query-results').append('<li>' + 'username:' + name + 'email:' + email + '</li>');
+    $.each(data, function(id, name) {
+      $('#query-results').append('<li>' + 'username:' + id + 'email:' + name + '</li>');
     });
   });
 });
