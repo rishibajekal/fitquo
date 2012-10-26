a116e1df81bff0c12b294ab1b9bd304500ad3f9f
Mohd Irtefa <irtefa1@illinois.edu>
2012-10-26 01:03:42 -0500

    solved syntax error

--- static/js/query_results.js
+++ static/js/query_results.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.getJSON('ajax/search', function(data) {
+  $.getJSON('api/search', function(data) {
     $.each(data, function(email, name) {
       $('#query-results').push('<li>' + 'username:' name + 'email:' + email + '</li>');
     });
