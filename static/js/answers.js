a2d0c5c09c6247f46603a139161a1b3b7bf88fc5
Mohd Irtefa <irtefa1@illinois.edu>
2012-11-28 23:42:31 -0600

    Added error handling to when there aren't answers

--- static/js/answers.js
+++ static/js/answers.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var question_id = $("#question_id").html();
     $.getJSON('/api/question/' + question_id, function(data) {
-      console.log(data[0]);
       $('#question').append(data[0]["content"]);
-      for (var i = 0; i < data.length; i++) {
-        $('#answers').append(data[1][i]["content"]);
+      if (data[1].length > 0) {
+        for (var i = 0; i < data.length; i++) {
+          $('#answers').append(data[1][i]["content"]);
+        }
       }
     });
 });
--- static/js/feed.js
+++ static/js/feed.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.getJSON('/api/feed', function(data){
         $.each(data, function(index){
             var question = data[index];
-            $('#questions').append("<p><h4>Question:</h4> <h4><a href='answers/" +
+            $('#questions').prepend("<p><h4>Question:</h4> <h4><a href='answers/" +
                 question["question_id"] + "'><large class='lead' id ='name'>" +
                 question["content"] + "</large></a></h4>");
         });
