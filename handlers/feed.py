8558685992d592252b3e6497b382b4e4fd59a054
Rishi Bajekal <rishi.bajekal@gmail.com>
2012-12-02 23:47:09 -0600

    No limit on feed. Pushing so much code. Rage mode :rage:

--- handlers/feed.py
+++ handlers/feed.py
@@ -7,7 +7,7 @@ class FeedHandler(BaseHandler):
 
     @asynchronous
     def get(self):
-        get_questions = """SELECT * FROM `Question` LIMIT 10"""
+        get_questions = """SELECT * FROM `Question`"""
         results = self.application.db.query(get_questions)
         self.set_header("Content-Type", "application/json")
         self.write(json.dumps(results))
