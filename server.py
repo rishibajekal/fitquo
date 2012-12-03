398da009fbbaa89d414ed9fcced29ebaec69b5a0
Mohd Irtefa <irtefa1@illinois.edu>
2012-12-03 03:58:36 -0600

    added redis config

--- server.py
+++ server.py
@@ -22,8 +22,8 @@ HOST = sys.argv[2]
 DB = sys.argv[3]
 USER = sys.argv[4]
 PASS = sys.argv[5]
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_HOST = "redis://redistogo:e91cf3f6188f66bfbc18bee9a1173daf@tench.redistogo.com:9243"
+REDIS_PORT = 9243
 REDIS_DB = 0
 define("port", default=PORT, help="run on the given port", type=int)
 define("debug", default=False, help="run tornado in debug mode", type=bool)
