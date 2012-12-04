a771b1127dd32be83277964801aaea96e5c2014c
Mohd Irtefa <irtefa1@illinois.edu>
2012-12-04 05:26:08 -0600

    added author and posted at

--- handlers/feed.py
+++ handlers/feed.py
@@ -10,5 +10,11 @@ class FeedHandler(BaseHandler):
         get_questions = """SELECT * FROM `Question`"""
         results = self.application.db.query(get_questions)
         self.set_header("Content-Type", "application/json")
+        for result in results:
+            user_id = result['user_id']
+            get_user = "SELECT user_name FROM User WHERE user_id = %s"
+            user = self.application.db.get(get_user, (user_id))
+            user_name = user['user_name']
+            result["author"] = user_name
         self.write(json.dumps(results))
         self.finish()
